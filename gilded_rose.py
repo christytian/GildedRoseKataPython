05f61c9835faafab1d1a01486e38e03e5c185783
christytian <tian.q@northeastern.edu>
2025-02-12 22:47:41 -0800

    Refactoring the code

--- gilded_rose.py
+++ gilded_rose.py
@@ -12,38 +12,80 @@ class Item:
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
 
 
-class GildedRose(object):
+# Base class for all item updaters
+class ItemUpdater:
+    def update_quality(self, item):
+        pass
 
+
+# Updater for normal items
+class NormalItemUpdater(ItemUpdater):
+    def update_quality(self, item):
+        if item.quality > 0:
+            item.quality -= 1
+        item.sell_in -= 1
+        if item.sell_in < 0 and item.quality > 0:
+            item.quality -= 1
+
+
+# Updater for "Aged Brie"
+class AgedBrieUpdater(ItemUpdater):
+    def update_quality(self, item):
+        if item.quality < 50:
+            item.quality += 1
+        item.sell_in -= 1
+        if item.sell_in < 0 and item.quality < 50:
+            item.quality += 1
+
+
+# Updater for "Sulfuras, Hand of Ragnaros"
+class SulfurasUpdater(ItemUpdater):
+    def update_quality(self, item):
+        pass  # Sulfuras does not change
+
+
+# Updater for "Backstage passes to a TAFKAL80ETC concert"
+class BackstagePassesUpdater(ItemUpdater):
+    def update_quality(self, item):
+        if item.sell_in <= 0:
+            item.quality = 0
+        elif item.sell_in <= 5:
+            item.quality += 3
+        elif item.sell_in <= 10:
+            item.quality += 2
+        else:
+            item.quality += 1
+        item.sell_in -= 1
+        if item.quality > 50:
+            item.quality = 50
+
+
+# Updater for "Conjured" items
+class ConjuredItemUpdater(ItemUpdater):
+    def update_quality(self, item):
+        if item.quality > 0:
+            item.quality -= 2
+        item.sell_in -= 1
+        if item.sell_in < 0 and item.quality > 0:
+            item.quality -= 2
+
+
+class GildedRose(object):
+    
     def __init__(self, items: list[Item]):
         # DO NOT CHANGE THIS ATTRIBUTE!!!
         self.items = items
+        # Map item names to their corresponding strategies
+        self.updaters = {
+            "Aged Brie": AgedBrieUpdater(),
+            "Sulfuras, Hand of Ragnaros": SulfurasUpdater(),
+            "Backstage passes to a TAFKAL80ETC concert": BackstagePassesUpdater(),
+            "Conjured": ConjuredItemUpdater(),
+        }
+
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+            # Use the appropriate updater for the item, defaulting to NormalItemUpdater
+            updater = self.updaters.get(item.name, NormalItemUpdater())
+            updater.update_quality(item)
--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -5,12 +5,50 @@ from gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
+    
     def test_foo(self):
         items = [Item("foo", 0, 0)]
+    
+    # example of test that checks for logical errors
+    def test_sulfuras_should_not_decrease_quality(self):
+        items = [Item("Sulfuras", 5, 80)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+        self.assertEquals("Sulfuras", items[0].name)
+        sulfuras_item = items[0]
+        self.assertEqual(79, sulfuras_item.quality)
+        self.assertEqual(4, sulfuras_item.sell_in)
+        self.assertEqual("Sulfuras", sulfuras_item.name)
 
+    # Ensures item quality is never negative after updating
+    def test_quality_never_negative(self):
+        items = [Item("Elixir of the Mongoose", 5, 0)]  # Quality starts at 0
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertGreaterEqual(items[0].quality, 0, "Quality should not be negative")
+    
+    # Ensures Aged Brie quality does not exceed 50 after updating
+    def test_aged_brie_quality_increases(self):
+        items = [Item("Aged Brie", 5, 50)]  # Aged Brie increases in quality
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(items[0].quality, 50, "Quality should never be greater than 50")
+
+
+    # Ensures Sulfuras quality and sell_in values remain unchanged after updating
+    def test_sulfuras_does_not_change(self):
+        items = [Item("Sulfuras, Hand of Ragnaros", 5, 80)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(items[0].quality, 80, "Sulfuras should always have quality 80")
+        self.assertEqual(items[0].sell_in, 5, "Sulfuras' sell_in should not decrease")
+
+    # Ensures calling a non-existent method on GildedRose raises an AttributeError
+    def test_gilded_rose_invalid_method(self):
+        items = [Item("Sulfuras, Hand of Ragnaros", 5, 80)]
+        gilded_rose = GildedRose(items)
+        with self.assertRaises(AttributeError, msg="Expected method get_item_list() to not exist"):
+            gilded_rose.get_item_list()
 
 if __name__ == '__main__':
     unittest.main()
